2bf93fd083db5899c3d97b857ed1b22d94639f9e
thebestprogrammer <jarodiking17@gmail.com>
2021-09-22 20:48:02 +0200

    Add project files.

---
+++ mathTrainer/MainWindow.xaml.cs
@@ -0,0 +1,277 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace mathTrainer
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        //constructor-----------------------------------------------------------
+        public MainWindow()
+        {
+            InitializeComponent();
+
+        }
+
+        //variables & arrays & objects----------------------------------------------------
+        private int level = 0;
+        private int correct = 0;
+        private int wrong = 0;
+        private int i = 0;
+        private int gameLength = 5;
+        private int playerAns = 0;
+
+        sumGeneration sumMaker = null;
+
+        //properties------------------------------------------------------------
+
+
+        //methods---------------------------------------------------------------
+        private void Start(object sender, RoutedEventArgs e)
+        {
+            if(level == 0)
+            {
+                MessageBox.Show("Please choose a level");
+                return;
+            }
+
+            if(playerName.Text == "")
+            {
+                MessageBox.Show("Please enter a name");
+                return;
+            } 
+
+            createSumObject(level, correct, wrong);
+
+            switch (sumMaker.Level)
+            {
+                case 1:
+                    number1.Content = sumMaker.nums1[i];
+                    if (sumMaker.operators[i] == 0)
+                        operator0.Content = "-";
+                    else
+                        operator0.Content = "+";
+                    number2.Content = sumMaker.nums2[i];
+                    break;
+
+                case 2:
+                    number1.Content = sumMaker.nums3[i];
+                    if (sumMaker.operators[i] == 0)
+                        operator0.Content = ":";
+                    else
+                        operator0.Content = "X";
+                    number2.Content = sumMaker.nums4[i];
+                    break;
+
+                case 3:
+                    number1.Content = sumMaker.nums5[i];
+                    switch (sumMaker.operators2[i])
+                    {
+                        case 0:
+                            operator0.Content = "+";
+                            break;
+
+                        case 1:
+                            operator0.Content = "-";
+                            break;
+
+                        case 2:
+                            operator0.Content = "X";
+                            break;
+
+                        case 3:
+                            operator0.Content = ":";
+                            break;
+                    }
+                    number2.Content = sumMaker.nums6[i];
+
+                    break;
+            }
+
+            MessageBox.Show("Start maar " + sumMaker.Name + " en veel success!");
+        }
+
+        private void Set_Level(object sender, RoutedEventArgs e)
+        {
+            var button = sender as Button;
+            int value = int.Parse(button.Tag.ToString());
+            level = value;
+            levelIndicator.Content = value;
+        }
+
+        private void Next_Question(object sender, RoutedEventArgs e)
+        {
+            if (sumMaker == null)
+                return;
+
+            switch (sumMaker.Level)
+            {
+                case 1:
+                    if (Check_Completion(i, gameLength))
+                        break;
+
+                    i++;
+
+                    number1.Content = sumMaker.nums1[i];
+                    if (sumMaker.operators[i] == 0)
+                        operator0.Content = "-";
+                    else
+                        operator0.Content = "+";
+                    number2.Content = sumMaker.nums2[i];
+
+                    break;
+
+                case 2:
+                    if (Check_Completion(i, gameLength))
+                        break;
+
+                    i++;
+
+                    number1.Content = sumMaker.nums3[i];
+                    if (sumMaker.operators1[i] == 0)
+                        operator0.Content = ":";
+                    else
+                        operator0.Content = "X";
+                    number2.Content = sumMaker.nums4[i];
+
+                    break;
+
+                case 3:
+                    if (Check_Completion(i, gameLength))
+                        break;
+
+                    i++;
+
+                    number1.Content = sumMaker.nums5[i];
+                    switch (sumMaker.operators2[i])
+                    {
+                        case 0:
+                            operator0.Content = "+";
+                            break;
+
+                        case 1:
+                            operator0.Content = "-";
+                            break;
+
+                        case 2:
+                            operator0.Content = "X";
+                            break;
+
+                        case 3:
+                            operator0.Content = ":";
+                            break;
+                    }
+                    number2.Content = sumMaker.nums6[i];
+                    
+                    break;
+            }
+        }
+
+        private void Check_Answer(object sender, RoutedEventArgs e)
+        {
+            if (sumMaker == null)
+                return;
+
+            if (!int.TryParse(playerAnswer.Text, out playerAns))
+            {
+                MessageBox.Show("please enter a number!");
+                return;
+            }
+
+            switch (sumMaker.Level)
+            {
+                case 1:
+                    if (Int32.Parse(playerAnswer.Text) == sumMaker.answers[i])
+                    {
+                        MessageBox.Show("correct!");
+                        sumMaker.Correct++;
+                        correctIndicator.Content = sumMaker.Correct;
+                    }
+                    else
+                    {
+                        MessageBox.Show("wrong! Answer was " + Convert.ToString(sumMaker.answers[i]));
+                        sumMaker.Wrong++;
+                        incorrectIndicator.Content = sumMaker.Wrong;
+                    }
+                    break;
+
+                case 2:
+                    if (Int32.Parse(playerAnswer.Text) == sumMaker.answers1[i])
+                    {
+                        MessageBox.Show("correct!");
+                        sumMaker.Correct++;
+                        correctIndicator.Content = sumMaker.Correct;
+                    }
+                    else
+                    {
+                        MessageBox.Show("wrong! Answer was: " + Convert.ToString(sumMaker.answers1[i]));
+                        sumMaker.Wrong++;
+                        incorrectIndicator.Content = sumMaker.Wrong;
+                    }
+                    break;
+
+                case 3:
+                    if (Int32.Parse(playerAnswer.Text) == sumMaker.answers2[i])
+                    {
+                        MessageBox.Show("correct!");
+                        sumMaker.Correct++;
+                        correctIndicator.Content = sumMaker.Correct;
+                    }
+                    else
+                    {
+                        MessageBox.Show("wrong! Answer was: " + Convert.ToString(sumMaker.answers2[i]));
+                        sumMaker.Wrong++;
+                        incorrectIndicator.Content = sumMaker.Wrong;
+                    }
+                    break;
+            }
+        }
+
+        private bool Check_Completion(int a, int lng)
+        {
+            MessageBox.Show(Convert.ToString(i) + " " + Convert.ToString(lng));
+
+            if (a == lng - 1)
+            {
+                MessageBox.Show("you finished the game!");
+                sumMaker = null;
+                i = 0;
+                level = 0;
+                correct = 0;
+                wrong = 0;
+                playerAns = 0;
+
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        private void createSumObject(int lvl, int cor, int wro)
+        {
+            if(level != 0)
+            {
+                sumMaker = new sumGeneration(lvl, cor, wro, gameLength);
+                sumMaker.Name = playerName.Text;
+                sumMaker.Level = level;
+                sumMaker.Start();
+            }
+        }
+    }
+}
---
+++ mathTrainer/sumGeneration.cs
@@ -0,0 +1,224 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace mathTrainer
+{
+    class sumGeneration
+    {
+        //constructor---------------------------------------------------------------------------
+        public sumGeneration(int lvl, int cor, int wro, int lng)
+        {
+            _level = lvl;
+            _correct = cor;
+            _wrong = wro;
+            _length = lng;
+
+            nums1 = new int[lng];
+            operators = new int[lng];
+            nums2 = new int[lng];
+            answers = new int[lng];
+
+            nums3 = new int[lng];
+            operators1 = new int[lng];
+            nums4 = new int[lng];
+            answers1 = new int[lng];
+
+            nums5 = new int[lng];
+            operators2 = new int[lng];
+            nums6 = new int[lng];
+            answers2 = new int[lng];
+        }
+
+
+        //variables & arrays--------------------------------------------------------------------
+        private int _level;
+        private int _correct;
+        private int _wrong;
+        private int _length;
+
+        private string _name;
+
+        //adding and subtraction
+        public int[] nums1;
+        public int[] operators;
+        public int[] nums2;
+        public int[] answers;
+
+        //multiplication
+        public int[] nums3;
+        public int[] operators1;
+        public int[] nums4;
+        public int[] answers1;
+
+        //All Operators
+        public int[] nums5;
+        public int[] operators2;
+        public int[] nums6;
+        public int[] answers2;
+
+        Random random = new Random();
+        //properties----------------------------------------------------------------------------
+        public int Level
+        {
+            get { return _level; }
+            set { _level = value; }
+        }
+
+        public int Correct
+        {
+            get { return _correct; }
+            set { _correct = value; }
+        }
+
+        public int Wrong
+        {
+            get { return _wrong; }
+            set { _wrong = value; }
+        }
+
+        public string Name
+        {
+            get { return _name; }
+            set { _name = value; }
+        }
+
+        public int gameLength
+        {
+            get { return _length; }
+            set { _length = value; }
+        }
+        //methods-------------------------------------------------------------------------------
+        public void Start()
+        {
+            createLevel(Level);
+        }
+        private void createLevel(int lvl)
+        {
+            int a;
+            switch (lvl)
+            {
+                //Generate subraction and adding equations
+                case 1:
+                    for (int i = 0; i < gameLength; i++)
+                    {
+                       nums1[i] = generateAddSubtract(lvl);
+                       operators[i] = generateAddSubtract(lvl, 1);
+                       nums2[i] = generateAddSubtract(lvl);
+                    }
+
+                    for (int i = 0; i < gameLength; i++)
+                    {
+                        if (operators[i] == 0)
+                        {
+                            answers[i] = nums1[i] - nums2[i];
+                        } else
+                        {
+                            answers[i] = nums1[i] + nums2[i];
+                        }
+                    }
+                    break;
+
+                //generate multiplication and division equations
+                case 2:
+                    for (int i = 0; i < gameLength; i++)
+                    {
+                        a = random.Next(2);
+                        generateMultiDiv(lvl, a, i);
+                    }
+                    break;
+
+                //generate all basic arithemtic equations
+                case 3:
+                    for (int i = 0; i < gameLength; i++)
+                    {
+                        a = random.Next(4);
+                        generateAll(lvl, a, i);
+                    }
+                    break;
+
+                default:
+
+                    break;
+                    
+            }
+        }
+
+        private int generateAddSubtract(int lvl, int op = 0)
+        {
+            //first level equations
+            if (lvl == 1 && op == 0)
+                return random.Next(100);
+            if (lvl == 1 && op == 1)
+                return random.Next(2);
+
+            return 0;
+        }
+
+        private void generateMultiDiv(int lvl, int op, int i)
+        {
+            if(op == 0)
+            {
+                nums3[i] = random.Next(11);
+                operators1[i] = 0;
+                int num = random.Next(11);
+                nums4[i] = num;
+
+                int ans = nums3[i] * nums4[i];
+
+                nums4[i] = ans;
+
+                answers1[i] = num;
+
+            } else
+            {
+                nums3[i] = random.Next(11);
+                operators1[i] = 1;
+                nums4[i] = random.Next(11);
+
+                answers1[i] = nums3[i] * nums4[i];
+            }
+        }
+
+        private void generateAll(int lvl, int op, int i)
+        {
+            switch (op)
+            {
+                case 0:
+                    nums5[i] = random.Next(101);
+                    operators2[i] = 0;
+                    nums6[i] = random.Next(101);
+                    answers2[i] = nums5[i] + nums6[i];
+                    break;
+
+                case 1:
+                    nums5[i] = random.Next(101);
+                    operators2[i] = 1;
+                    nums6[i] = random.Next(101);
+                    answers2[i] = nums5[i] - nums6[i];
+                    break;
+
+                case 2:
+                    nums5[i] = random.Next(11);
+                    operators2[i] = 2;
+                    nums6[i] = random.Next(11);
+                    answers2[i] = nums5[i] * nums6[i];
+                    break;
+
+                case 3:
+                    nums5[i] = random.Next(11);
+                    operators2[i] = 3;
+                    int num = random.Next(11);
+                    nums6[i] = num;
+
+                    int ans = nums5[i] * nums6[i];
+
+                    nums6[i] = ans;
+
+                    answers2[i] = num;
+                    break;
+            }
+        }
+
+    }
+}
